b5f5bb891aaf93a26f81c828d2d793ca6ca24043
F.H. SAGOR <sagorpstu17@gmail.com>
2020-05-27 17:02:29 +0600

    Update active.js

--- js/active.js
+++ js/active.js
@@ -1,24 +1,18 @@
 $(document).ready(function () {
   // SideBar
-  $("#sideBarOpen").click(function () {
-    $("#sideBarClose").show();
-    $("#sideBarOpen").hide();
-    $("#sideBar").animate(
-      {
-        width: "200px",
-      },
-      400
-    );
-  });
 
-  $("#sideBarClose").click(function () {
-    $("#sideBarClose").hide();
-    $("#sideBarOpen").show();
-    $("#sideBar").animate(
-      {
-        width: "0px",
-      },
-      400
-    );
+  $("#sideBarNavOpen").click(function () {
+    $("#sideBarNav").animate({
+      width: "200px",
+    });
+    $("#sideBarNavClose").show();
+    $(this).hide();
+  });
+  $("#sideBarNavClose").click(function () {
+    $("#sideBarNav").animate({
+      width: "0px",
+    });
+    $("#sideBarNavOpen").show();
+    $(this).hide();
   });
 });
